ec2ed35a0d0be2126cf574cbd54e09dd26dac39e
Ahmed <ahmedmumdoohmax@gmail.com>
2025-12-29 07:07:36 +0200

    blink on press

---
+++ blinkonbutton/blinkonbutton.ino
@@ -0,0 +1,24 @@
+#define BUTTON_PIN 2
+#define LED 11
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  if(digitalRead(BUTTON_PIN) == HIGH){
+  Serial.println("Button is pressed!");
+    digitalWrite(LED, HIGH);
+  }
+
+  else{
+   digitalWrite(LED, LOW); 
+
+  }
+  // put your main code here, to run repeatedly:
+  delay(250);
+}
