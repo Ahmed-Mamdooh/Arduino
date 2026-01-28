50b3cb52d1e629f6101f910e7397874504334885
Ahmed <ahmedmamdoohsd@gmail.com>
2026-01-28 09:33:37 +0200

    Step 3: lock application when obstacle too close

--- obstacle_detection_project/obstacle_detection_project.ino
+++ obstacle_detection_project/obstacle_detection_project.ino
@@ -1,7 +1,10 @@
 #define  ECHO_PIN 3
 #define TRIGGER_PIN 4
 #define WARNING_LED_PIN 11
-
+#define LOCK_DISTANCE 10.0
+#define ERROR_LED_PIN 12
+// #define
+// Ultrasonic
 unsigned long lastTimeUltrasonicTrigger = millis();
 unsigned long ultrasonicTriggerDelay = 60;
 
@@ -11,9 +14,17 @@ volatile unsigned long pulseInTimeEnd;
 volatile bool newDistanceAvailable = false;
 double prevDistance = 400;
 
+// warning LED
 unsigned long lastTimeWarningLEDBlinked = millis();
 unsigned long warningLEDDelay = 500;
 byte warningLEDState = LOW;
+
+
+// error LED
+unsigned long lastTimeErrorLEDBlinked = millis();
+unsigned long errorLEDDelay = 300;
+byte errorLEDState = LOW;
+bool isLocked = false;
 
 void triggerUltrasonicSensor(){
 digitalWrite(TRIGGER_PIN, LOW);
@@ -43,6 +54,11 @@ void echoPinInterrupt(){
   }
 }
 
+void toggleErrorLED(){
+  errorLEDState = (errorLEDState == LOW) ? HIGH : LOW;
+  digitalWrite(ERROR_LED_PIN, errorLEDState);
+}
+
 void toggleWarningLED(){
   warningLEDState = (warningLEDState == LOW) ? HIGH : LOW;
   digitalWrite(WARNING_LED_PIN, warningLEDState);
@@ -51,10 +67,23 @@ void setWarningLEDBlinkRateFromDistance(double distance){
 warningLEDDelay = distance * 4;
 }
 
+
+void lock(){
+  if(!isLocked){
+    isLocked = true;
+    warningLEDState= LOW;
+    errorLEDState = LOW;
+  }
+
+
+
+}
+
 void setup() {
 Serial.begin(115200);
 pinMode(ECHO_PIN, INPUT);
 pinMode(WARNING_LED_PIN, OUTPUT);
+pinMode(ERROR_LED_PIN, OUTPUT);
 
 pinMode(TRIGGER_PIN, OUTPUT);
 attachInterrupt(digitalPinToInterrupt(ECHO_PIN),
@@ -65,10 +94,20 @@ CHANGE);
 
 void loop() {
 unsigned long timeNow = millis();
-  if(timeNow - lastTimeUltrasonicTrigger > ultrasonicTriggerDelay){
+
+  if(isLocked){
+    if(timeNow - lastTimeErrorLEDBlinked > errorLEDDelay){
+      lastTimeErrorLEDBlinked += errorLEDDelay;
+      toggleErrorLED();
+      toggleWarningLED();
+    }
+  }
+  else{ 
+    if(timeNow - lastTimeUltrasonicTrigger > ultrasonicTriggerDelay){
     lastTimeUltrasonicTrigger += ultrasonicTriggerDelay;
     triggerUltrasonicSensor();
   }
+
 
   if(timeNow - lastTimeWarningLEDBlinked > warningLEDDelay){
     lastTimeWarningLEDBlinked += warningLEDDelay;
@@ -76,9 +115,13 @@ unsigned long timeNow = millis();
   }
 
   if(newDistanceAvailable){
-double distance = getUltrasonicDistance();
-setWarningLEDBlinkRateFromDistance(distance);
-Serial.println(distance);
+  double distance = getUltrasonicDistance();
+  setWarningLEDBlinkRateFromDistance(distance);
+  Serial.println(distance);
+  if(distance < LOCK_DISTANCE){
+    lock();
+}
   }
+    }
 
 }
